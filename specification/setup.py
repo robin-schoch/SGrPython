531d6b44e63b0e079b884fb8c958594bbab3c09f
robin schoch <20967248+robin-schoch@users.noreply.github.com>
2024-06-13 07:11:31 +0200

    fixed issue with wrong encoding of authentication body

---
+++ specification/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SGrSpecificationPythontks4r',
+    version='0.1.2',
+    packages=find_packages(),
+    url='https://github.com/robin-schoch/SGrPython',
+    license='',
+    author='robin',
+    author_email='',
+    description='',
+)

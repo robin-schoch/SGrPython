6b4e86e6a5a5d671efb32e877429b0614cdcd0b3
GitHub Action <action@github.com>
2024-06-19 09:18:01 +0000

    Bump version to 0.2.2

--- specification/setup.py
+++ specification/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SGrSpecificationPythontks4r',
-    version='{{tag}}',
+    version='0.2.2',
     packages=find_packages(),
     url='https://github.com/robin-schoch/SGrPython',
     license='',
